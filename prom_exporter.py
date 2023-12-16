9dffd5231a308b66ab9d7d9221d08e3cd6fc6db7
molnars <szabolcs.molnar@gmail.com>
2023-12-17 01:07:13 +0400

    Update prom_exporter.py

--- prom_exporter.py
+++ prom_exporter.py
@@ -4,38 +4,33 @@ import os
 import requests
 import time #import sleep
 import subprocess
-from http.server import BaseHTTPRequestHandler, HTTPServer
+#from http.server import BaseHTTPRequestHandler, HTTPServer
 from prometheus_client import CollectorRegistry, start_http_server, Gauge, Histogram
 
+freq = 60
 registry = CollectorRegistry()
 inotify_handle_count = Gauge("inotify_handle_total", "total count of process inotify handles" ) #, registry=registry)   #, ["inotify_count"],[""])
 #hitl_psql_health_request_time = Histogram('hitl_psql_health_request_time', 'PSQL connection response time (seconds)')
 
-class HTTPRequestHandler(BaseHTTPRequestHandler):
+def plog(severity, message, value):
+    print(now.strftime("%d/%m/%Y %H:%M:%S"), " ", severity, " ", message, ": ", value)
     
-    def do_GET(self):
-        if self.path == '/metrics':
-            self.send_response(200)
-            self.send_header('Content-type','text/html')
-            self.end_headers()
-            self.wfile.write(bytes("<b> Hello World !</b>", "utf-8"))
-#            request_counter.labels(status_code='200', instance=platform.node()).inc()
-#            get_metrics()
-        else:
-            self.send_error(404)
-#            request_counter.labels(status_code='404', instance=platform.node()).inc()
-
 def get_metrics():
 #   inotify_handle_count.set(
    iTotal=subprocess.check_output('for foo in /proc/*/fd/*; do readlink -f $foo; done | grep inotify | wc -l', shell=True, text=True)
-   #print(iTotal)
+   #print(now.strftime("%d/%m/%Y %H:%M:%S"), " [INFO] inotify_handle_total: ", iTotal)
+   plog("INFO","inotify_handle_total", iTotal)
    inotify_handle_count.set(iTotal)
             
             
 if __name__ == '__main__':
     start_http_server(9000)
 #    webServer = HTTPServer(("localhost", 9000), HTTPRequestHandler).serve_forever()
+    freq = os.environ.get('frequency', freq)
+    
+    plog("INFO","Collection starting","")
+    plog("INFO","frquency set at", freq)
     inotify_handle_count.set(-1)    
     while True:
         get_metrics()
-        time.sleep(15)
+        time.sleep(30)
