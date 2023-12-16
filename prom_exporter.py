e920f997ca72a0f5ae8aaf3d0de694a96170afa3
molnars <szabolcs.molnar@gmail.com>
2023-12-17 01:26:28 +0400

    fix timer

--- prom_exporter.py
+++ prom_exporter.py
@@ -3,18 +3,17 @@
 import os
 import requests
 import time #import sleep
-import datetime
+from datetime import datetime
 import subprocess
 #from http.server import BaseHTTPRequestHandler, HTTPServer
 from prometheus_client import CollectorRegistry, start_http_server, Gauge, Histogram
 
-freq = 60
 registry = CollectorRegistry()
 inotify_handle_count = Gauge("inotify_handle_total", "total count of process inotify handles" ) #, registry=registry)   #, ["inotify_count"],[""])
 #hitl_psql_health_request_time = Histogram('hitl_psql_health_request_time', 'PSQL connection response time (seconds)')
 
 def plog(severity, message, value):
-    print(now.strftime("%d/%m/%Y %H:%M:%S"), " ", severity, " ", message, ": ", value)
+    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),'[',severity.upper(),']',message,":", value)
     
 def get_metrics():
 #   inotify_handle_count.set(
@@ -27,11 +26,11 @@ def get_metrics():
 if __name__ == '__main__':
     start_http_server(9000)
 #    webServer = HTTPServer(("localhost", 9000), HTTPRequestHandler).serve_forever()
-    freq = os.environ.get('frequency', freq)
+    freq = os.environ.get('frequency', 60)
     
-    plog("INFO","Collection starting","")
-    plog("INFO","frquency set at", freq)
+    plog('INFO',"Collection starting","")
+    plog('INFO',"Frequency set at", freq)
     inotify_handle_count.set(-1)    
     while True:
         get_metrics()
-        time.sleep(30)
+        time.sleep(freq)
